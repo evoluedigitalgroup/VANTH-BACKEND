124e7c106e3eda73acec0d678def21fec808e8fd
Chirag Lathiya <clathiya.work@gmail.com>
2024-01-14 02:41:25 +0530

    Resolving the case mismatch issue

---
+++ server/helpers/seedingData/documentFile.js
@@ -0,0 +1,19 @@
+const seedingDocumentFile = [
+  {
+    label: "cnpj",
+    title: "CNPJ",
+    type: "cnpj",
+  },
+  {
+    label: "cpf_dos_socios",
+    title: "CPF dos sócios",
+    type: "cpf_dos_socios",
+  },
+  {
+    label: "contrato_social",
+    title: "Contrato social",
+    type: "contrato_social",
+  }
+];
+
+export default seedingDocumentFile;
