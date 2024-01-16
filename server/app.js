7cfd7e4c6239f1d7dff3be641327d352c3a2fff3
Chirag Lathiya <clathiya.work@gmail.com>
2024-01-16 18:42:29 +0530

    Removed CORS setup, it doesn't needed

--- server/app.js
+++ server/app.js
@@ -17,10 +17,7 @@ import packageJson from "../package.json";
 var app = express();
 
 app.use(logger("dev"));
-app.use(cors({
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
-}));
+app.use(cors());
 app.use("temp", express.static(__dirname + "/server" + "/temp"));
 
 app.use(express.json({ limit: "50mb" }));
